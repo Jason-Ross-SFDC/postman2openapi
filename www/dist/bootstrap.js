59da2fdb3ff1253a140fbb74ded7fb790fb1767c
Kevin Swiber <kswiber@gmail.com>
2020-08-26 18:47:57 -0700

    Adding formatted online conversion for GitHub Pages.
    Signed-off-by: Kevin Swiber <kswiber@gmail.com>

--- www/dist/bootstrap.js
+++ www/dist/bootstrap.js
@@ -52,14 +52,14 @@
 /******/ 	function promiseResolve() { return Promise.resolve(); }
 /******/
 /******/ 	var wasmImportObjects = {
-/******/ 		"../../pkg/postman2openapi_bg.wasm": function() {
+/******/ 		"../pkg/postman2openapi_bg.wasm": function() {
 /******/ 			return {
 /******/ 				"./postman2openapi_bg.js": {
 /******/ 					"__wbindgen_string_new": function(p0i32,p1i32) {
-/******/ 						return installedModules["../../pkg/postman2openapi_bg.js"].exports["__wbindgen_string_new"](p0i32,p1i32);
+/******/ 						return installedModules["../pkg/postman2openapi_bg.js"].exports["__wbindgen_string_new"](p0i32,p1i32);
 /******/ 					},
 /******/ 					"__wbindgen_rethrow": function(p0i32) {
-/******/ 						return installedModules["../../pkg/postman2openapi_bg.js"].exports["__wbindgen_rethrow"](p0i32);
+/******/ 						return installedModules["../pkg/postman2openapi_bg.js"].exports["__wbindgen_rethrow"](p0i32);
 /******/ 					}
 /******/ 				}
 /******/ 			};
@@ -152,7 +152,7 @@
 /******/
 /******/ 		// Fetch + compile chunk loading for webassembly
 /******/
-/******/ 		var wasmModules = {"0":["../../pkg/postman2openapi_bg.wasm"]}[chunkId] || [];
+/******/ 		var wasmModules = {"0":["../pkg/postman2openapi_bg.wasm"]}[chunkId] || [];
 /******/
 /******/ 		wasmModules.forEach(function(wasmModuleId) {
 /******/ 			var installedWasmModuleData = installedWasmModules[wasmModuleId];
@@ -162,7 +162,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../../pkg/postman2openapi_bg.wasm":"97fd5b8019e63a4e54c1"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/postman2openapi_bg.wasm":"7cf93fc5febc6b83b7d8"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
@@ -263,7 +263,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// A dependency graph that contains any wasm must all be imported\n// asynchronously. This `bootstrap.js` file does the single async import, so\n// that no one else needs to worry about it again.\n__webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! ./index.js */ \"./index.js\"))\n  .catch(e => console.error(\"Error importing `index.js`:\", e));\n\n\n//# sourceURL=webpack:///./bootstrap.js?");
+eval("// A dependency graph that contains any wasm must all be imported\n// asynchronously. This `bootstrap.js` file does the single async import, so\n// that no one else needs to worry about it again.\nPromise.all(/*! import() */[__webpack_require__.e(1), __webpack_require__.e(0)]).then(__webpack_require__.bind(null, /*! ./index.js */ \"./index.js\"))\n  .catch(e => console.error(\"Error importing `index.js`:\", e));\n\n\n//# sourceURL=webpack:///./bootstrap.js?");
 
 /***/ })
 
--- www/index.js
+++ www/index.js
@@ -1,11 +1,31 @@
 import { default as collection } from './collection';
+import CodeMirror from 'codemirror/lib/codemirror.js';
 import * as postman2openapi from "postman2openapi";
 
-const postman = JSON.stringify(collection);
-const openapi = postman2openapi.transpile(postman, 'yaml');
+const postmanElement = CodeMirror.fromTextArea(document.getElementById('postman'), {
+    lineNumbers: true
+});
 
-const textarea = document.createElement('textarea');
-textarea.style = "width: 100%; height: 700px;";
-textarea.value = openapi;
+postmanElement.setValue(JSON.stringify(collection, 0, 2));
 
-document.body.appendChild(textarea);
+postmanElement.on('change', (_) => {
+    update();
+});
+
+const openapiElement = CodeMirror.fromTextArea(document.getElementById('openapi'), {
+    readOnly: true
+});
+
+const update = () => {
+    console.log('updating');
+    console.log(postmanElement);
+    const postman = postmanElement.getValue();
+    try {
+        const openapi = postman2openapi.transpile(postman, 'yaml');
+        openapiElement.setValue(openapi);
+    } catch (e) {
+        openapiElement.setValue(e);
+    }
+};
+
+update();
